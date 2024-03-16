00da2f9618296b408addd20e8296adc2e64f86e0
HuongLanTo <tohuonglan1705@gmail.com>
2024-03-16 13:09:18 -0500

    Initial project

---
+++ src/main/java/group2/EAProject/EaProjectApplication.java
@@ -0,0 +1,13 @@
+package group2.EAProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group2/EAProject/EaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package group2.EAProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
