088ef0ffbced67dce91e958dbd10c96b0f2015d1
1Aayush3 <joshiaayushraj@gmail.com>
2024-03-18 13:42:14 -0500

    role setters and getters added

--- cs544-202403-project/src/main/java/edu/miu/cs/cs544/domain/Role.java
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/domain/Role.java
@@ -1,15 +1,11 @@
 package edu.miu.cs.cs544.domain;
 
 import jakarta.persistence.*;
-import lombok.Data;
-
 import java.io.Serializable;
 
 @Entity
-@Data
 public class Role implements Serializable {
     private static final long serialVersionUID = 1L;
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer roleId;
@@ -17,4 +13,19 @@ public class Role implements Serializable {
     @Column(name = "roleType")
     private String roleType;
 
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(String roleType) {
+        this.roleType = roleType;
+    }
 }
