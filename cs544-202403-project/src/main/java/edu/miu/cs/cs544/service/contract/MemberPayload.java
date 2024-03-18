ea3d040059c966cb0c1fa8702d43f4a6ff6ad23a
1Aayush3 <joshiaayushraj@gmail.com>
2024-03-18 10:07:16 -0500

    member entity intialized

--- cs544-202403-project/src/main/java/edu/miu/cs/cs544/service/contract/MemberPayload.java
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/service/contract/MemberPayload.java
@@ -2,15 +2,19 @@ package edu.miu.cs.cs544.service.contract;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
 import lombok.Data;
 
 @Data
 public class MemberPayload implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer memberId;
-	
-	private String name;
+	private String fname;
+	private String lname;
+	private String email;
+	private Integer barCode;
 
 }
