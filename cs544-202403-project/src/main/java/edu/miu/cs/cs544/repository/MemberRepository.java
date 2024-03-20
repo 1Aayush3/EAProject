fdec4340813293e27e870a6661684458211848bc
HuongLanTo <tohuonglan1705@gmail.com>
2024-03-20 16:16:55 -0500

    Rebase staging

--- cs544-202403-project/src/main/java/edu/miu/cs/cs544/repository/MemberRepository.java
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/repository/MemberRepository.java
@@ -17,8 +17,8 @@ public interface MemberRepository extends BaseRepository<Member, Integer>{
     @Query(value = "select a.session from Attendance a where a.member.memberId = :memberId")
     List<Session> memberAttendanceForEvent(Integer memberId);
 
-    @Query(value = "select s.AccountType, count(s.AccountType) from Attendance a join Session s on a.sessionId = s.sessionId " +
-                    "join Event e on s.event_id = e.id join Scan sc on e.id = sc.event_id " +
-                    "where a.memberId = :memberId group by s.account_type", nativeQuery = true)
+    @Query(value = "select sc.accountType as AccountType, count(sc.accountType) as No from Attendance a join Session s on a.sessionId = s.sessionId " +
+                    "join Event e on s.event_id = e.id join Scanner sc on e.id = sc.event_id " +
+                    "where a.memberId = :memberId group by sc.accountType", nativeQuery = true)
     List<Object[]> calculateAttendanceByMemberId(@Param("memberId") Integer memberId);
 }
