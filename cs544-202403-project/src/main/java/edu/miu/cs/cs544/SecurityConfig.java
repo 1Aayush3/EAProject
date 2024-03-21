f5f9537e1fc0431f0bf28e92fbe0912d3fd63eae
HuongLanTo <tohuonglan1705@gmail.com>
2024-03-21 12:26:37 -0500

    Set up OAuth2

--- cs544-202403-project/src/main/java/edu/miu/cs/cs544/SecurityConfig.java
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/SecurityConfig.java
@@ -2,6 +2,7 @@ package edu.miu.cs.cs544;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,7 +12,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(Customizer.withDefaults());
         return http.build();
     }
 
